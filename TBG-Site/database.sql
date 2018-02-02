5476e41bafbedbea2ef3e57a877874dd686218f2
jens molenaers <jensmolenaers@hotmail.com>
2018-02-02 17:11:41 +0100

    Update database
    Toevoegen van gegevens voor de video's.

--- TBG-Site/database.sql
+++ TBG-Site/database.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS `hoofdpag` (
 -- Dumpen data van tabel tbg.hoofdpag: ~1 rows (ongeveer)
 /*!40000 ALTER TABLE `hoofdpag` DISABLE KEYS */;
 INSERT INTO `hoofdpag` (`gegevensId`, `vidLink`, `tekstFrontPage`) VALUES
-	(1, 'https://www.youtube.com/watch?v=GPpUGRfaHqs&t=1781s', '<strong>! Nieuwe website in aanbouw !</strong><br/><br/>\r\n                Hallo en welkom op de website van The BelgiumGames.<br/>\r\n                Deze website gaat over de YouTuber Jens en zijn video\'s.<br/><br/>\r\n                Wie is Jens? Hij is een Belgische YouTuber die voor jouw video\'s maakt over Minecraft en andere games.<br/>\r\n                Wat kun je verwachten op deze website? Meer info over de video\'s van Jens, zoals wanneer er een video uitkomt en dan ook nog meer info over Jens zelf.<br/>\r\n                <br/>\r\n                De video\'s die ik maak zijn vaak tutorials en behulpzame video\'s.<br/>\r\n                Geniet van de video\'s en ook van je dag.<br/><br/>\r\n                \r\n                Meer info over opkomende video\'s kan je vinden op de <a href="https://www.thebelgiumgames.be/extra.html">aankondigings</a> pagina.<br/><br/>\r\n                \r\n                Dit is de vernieuwde site van Thebelgiumgames gesponserd door Badskinsgarden.<br/><br/>');
+	(1, 'https://www.youtube.com/watch?v=d1bYO3uzs40', 'Hallo en welkom op de website van The BelgiumGames.<br/>\r\n                Deze website gaat over de YouTuber Jens en zijn video\'s.<br/><br/>\r\n                Wie is Jens? Hij is een Belgische YouTuber die voor jouw video\'s maakt over Minecraft en andere games.<br/>\r\n                Wat kun je verwachten op deze website? Meer info over de video\'s van Jens, zoals wanneer er een video uitkomt en dan ook nog meer info over Jens zelf.<br/>\r\n                <br/>\r\n                De video\'s die ik maak zijn vaak tutorials en behulpzame video\'s.<br/>\r\n                Geniet van de video\'s en ook van je dag.<br/><br/>\r\n                \r\n                Meer info over opkomende video\'s kan je vinden op de <a href="https://www.thebelgiumgames.be/extra.html">aankondigings</a> pagina.<br/><br/>\r\n                \r\n                Dit is de vernieuwde site van Thebelgiumgames gesponserd door Badskinsgarden.<br/><br/>');
 /*!40000 ALTER TABLE `hoofdpag` ENABLE KEYS */;
 
 -- Structuur van  tabel tbg.video wordt geschreven
@@ -35,17 +35,39 @@ CREATE TABLE IF NOT EXISTS `video` (
   `VideoId` int(11) NOT NULL AUTO_INCREMENT,
   `VideoLink` text NOT NULL,
   PRIMARY KEY (`VideoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
--- Dumpen data van tabel tbg.video: ~6 rows (ongeveer)
+-- Dumpen data van tabel tbg.video: ~5 rows (ongeveer)
 /*!40000 ALTER TABLE `video` DISABLE KEYS */;
 INSERT INTO `video` (`VideoId`, `VideoLink`) VALUES
-	(1, 'https://www.youtube.com/watch?v=DVQiHg2TrHA&t=403s'),
-	(2, 'https://www.youtube.com/watch?v=PGQM4rQp73Y'),
-	(3, 'https://www.youtube.com/watch?v=XOur7W3xfJE&t=695s'),
-	(4, 'https://www.youtube.com/watch?v=fLNXxHY4I8I'),
-	(5, 'https://www.youtube.com/watch?v=p44TszdiiA4'),
-	(6, 'https://www.youtube.com/watch?v=GPpUGRfaHqs&t=1614s');
+	(1, 'https://www.youtube.com/watch?v=d1bYO3uzs40'),
+	(2, 'https://www.youtube.com/watch?v=eUfcjirD6RY'),
+	(3, 'https://www.youtube.com/watch?v=pf8DSgO2ZpY'),
+	(4, 'https://www.youtube.com/watch?v=DVvGPWVWWUI'),
+	(5, 'https://www.youtube.com/watch?v=fLNXxHY4I8I'),
+	(6, 'https://www.youtube.com/watch?v=73XkMcRup5s'),
+	(7, 'https://www.youtube.com/watch?v=0Mbzt20lr6c&t=1s'),
+	(8, 'https://www.youtube.com/watch?v=2p7qvjxCL-k'),
+	(9, 'https://www.youtube.com/watch?v=4avmhJSpWZ0'),
+	(10, 'https://www.youtube.com/watch?v=DVQiHg2TrHA&t=1s'),
+	(11, 'https://www.youtube.com/watch?v=lgc8DmSjRGE&list=PLZDG9piGctYe1VLNPJTetWFpomJQKYkzB&index=5'),
+	(12, 'https://www.youtube.com/watch?v=MPpMrL8iPhs&list=PLZDG9piGctYe1VLNPJTetWFpomJQKYkzB&index=3'),
+	(13, 'https://www.youtube.com/watch?v=mm-1JPcc-eU&list=PLZDG9piGctYe1VLNPJTetWFpomJQKYkzB&index=2'),
+	(14, 'https://www.youtube.com/watch?v=UmxxAVAVYNY&list=PLZDG9piGctYe1VLNPJTetWFpomJQKYkzB&index=1'),
+	(15, 'https://www.youtube.com/watch?v=mdiTb07rDis&list=PLZDG9piGctYd8AoRZYRjSfRkR6yQU8UE1&index=4'),
+	(16, 'https://www.youtube.com/watch?v=XuoXTfSDtJc&list=PLZDG9piGctYd8AoRZYRjSfRkR6yQU8UE1&index=2'),
+	(17, 'https://www.youtube.com/watch?v=tGhj_oxL7e0&list=PLZDG9piGctYd8AoRZYRjSfRkR6yQU8UE1&index=1'),
+	(18, 'https://www.youtube.com/watch?v=qLBSnXenLoc&list=PLZDG9piGctYdehjTgYJtSRQ_7YrYA_uSK&index=3'),
+	(19, 'https://www.youtube.com/watch?v=BktjcMmzGg4&list=PLZDG9piGctYdehjTgYJtSRQ_7YrYA_uSK&index=2'),
+	(20, 'https://www.youtube.com/watch?v=eUfcjirD6RY&list=PLZDG9piGctYdehjTgYJtSRQ_7YrYA_uSK&index=1'),
+	(21, 'https://www.youtube.com/watch?v=cb7bmASw2rM&list=PLZDG9piGctYfVEPketZch5EdpdzQ_7NuL&index=21'),
+	(22, 'https://www.youtube.com/watch?v=3OB3KhTfzVQ&list=PLZDG9piGctYfVEPketZch5EdpdzQ_7NuL&index=30'),
+	(23, 'https://www.youtube.com/watch?v=zWz8P8GMtAo&list=PLZDG9piGctYfVEPketZch5EdpdzQ_7NuL&index=36'),
+	(24, 'https://www.youtube.com/watch?v=ehnBVQ990hw&list=PLZDG9piGctYfVEPketZch5EdpdzQ_7NuL&index=47'),
+	(25, 'https://www.youtube.com/watch?v=pbq0oMZ9u4A&index=1&list=PLZDG9piGctYe_jy1LNoCqSC3j_rrpEviz'),
+	(26, 'https://www.youtube.com/watch?v=l5a7b1yWlKU&index=2&list=PLZDG9piGctYe_jy1LNoCqSC3j_rrpEviz'),
+	(27, 'https://www.youtube.com/watch?v=yNQ_701uumU&index=3&list=PLZDG9piGctYe_jy1LNoCqSC3j_rrpEviz'),
+	(28, 'https://www.youtube.com/watch?v=GcKLjHezuFs&index=4&list=PLZDG9piGctYe_jy1LNoCqSC3j_rrpEviz');
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
