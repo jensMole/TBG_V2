fcc3ebe5e3da704755a0949451b96d4dff4167ce
DJJASPER21 <djjasper21@gmail.com>
2018-03-03 20:00:19 +0100

    Erro's pagina's
    403 en 404 zijn geupdate met meer uitleg.

--- TBG-Site/resources/views/errors/403.blade.php
+++ TBG-Site/resources/views/errors/403.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="nl">
     <head>
-    <title>Oeps iets misgegaan</title>
+    <title>Toegang geweigerd</title>
     <link rel="shortcut icon" href="afbeeldingen/youtubeTBG.png"/>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,8 +32,8 @@
     </head>
     <body>
 
-        <h1>Helaas is er iets misgelopen </h1>
-        <h3>(errorcode: 404 PAGE_NOT_FOUND)</h3>
+        <h1>Helaas heb je geen toegang voor deze pagina ...</h1>
+        <h3>(errorcode: 403 NO_ACCES)</h3>
         <p style="text-align:center; font-size: 150%">
             Probeer het later nog eens of probeer Jens of Jasper te bereiken via <a target="_blank" href="https://www.youtube.com/c/TheBelgiumGamesTBG">YouTube</a> of via <a href="mailto:the.belgiumg@gmail.com">Mail</a>. 
         </p>
