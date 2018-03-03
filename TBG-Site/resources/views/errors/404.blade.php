85a68a8755900a9caa9260e1f78b5fed59b65887
jens molenaers <jensmolenaers@hotmail.com>
2018-03-03 16:00:58 +0100

    404 page
    Custom 404 page aangemaakt.

---
+++ TBG-Site/resources/views/errors/404.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+    <title>Oeps iets misgegaan</title>
+    <link rel="shortcut icon" href="afbeeldingen/youtubeTBG.png"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Welkom bij TheBelgiumgames. Ik ben Jens en ik maak video's over minecraft en andere spelletjes. Op deze site vind je een verdeling van al de video's en welke video's binnenkort komen. Geniet van de video's" />
+    <meta name="keywords" content="thebelgiumgames, the belgiumgames, The Belgiumgames, TheBelgiumgames, youtube, minecraft, mc, tutorials, videos, adventures, redstone, survival, respawn, network" />
+    <meta name="author" content="Jens, Jasper"/>
+
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="/css/cssHoofd.css" type="text/css" rel="stylesheet"/>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <style>
+
+        body {
+            background-image: url("afbeeldingen/404.png");
+            color: white;
+        }
+
+    </style>
+
+    </head>
+    <body>
+
+        <h1>Helaas is er iets misgelopen </h1>
+        <p style="text-align:center; font-size: 150%">
+            Probeer het later nog eens of probeer Jens of Jasper te bereiken via <a target="_blank" href="https://www.youtube.com/c/TheBelgiumGamesTBG">YouTube</a> of via <a href="mailto:the.belgiumg@gmail.com">Mail</a>. 
+        </p>
+
+        <footer style="bottom:0px; position:absolute;">
+            <p style="text-align:center">Probeer later eens opnieuw of ga naar de<a href="https://www.thebelgiumgames.be/">home page</a>.</p>
+
+            <!-- Licentie -->
+            <a class="copy" rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/deed.nl">
+                <img alt="Creative Commons-Licentie" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+            </a>
+            <div style="text-align:center" class="hidden-xs">
+                Deze website valt onder een
+                <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/deed.nl">Creative Commons Naamsvermelding-GelijkDelen 4.0 Internationaal-licentie.
+                </a>&copy; 2017 - 2018 TheBelgiumGames
+            </div>
+            <div style="text-align:center" class="hidden-xs">
+                    Domein gesponsord door:
+                    <a rel="license" target="_blank" href="https://www.badskinsgarden.be">Badskinsgarden</a>
+            </div>
+            <div style="text-align:center; padding-top: 10px;" class="visible-xs">
+                    Sponsor:
+                    <a rel="license" target="_blank" href="https://www.badskinsgarden.be">Badskinsgarden</a>
+            </div>
+        </footer>	
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="/js/bootstrap.min.js"></script>
+
+    </body>
+</html>
