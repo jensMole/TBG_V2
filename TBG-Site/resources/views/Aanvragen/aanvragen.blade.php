6a726298dbde331760d4661e2d578816e65955ec
BuildTools <jensmolenaers@hotmail.com>
2018-08-12 13:17:23 +0200

    upadte

--- TBG-Site/resources/views/Aanvragen/aanvragen.blade.php
+++ TBG-Site/resources/views/Aanvragen/aanvragen.blade.php
@@ -20,14 +20,30 @@
 
 </div>
 
-<div id="content">
-    <div class="row col-sm-6">
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis imperdiet sapien, viverra suscipit ex. Quisque semper nunc bibendum purus suscipit tincidunt. Cras auctor nisl eu dolor imperdiet semper. In pretium malesuada dignissim. Nulla facilisi. Mauris a ligula rutrum, maximus dolor in, tincidunt turpis. Vestibulum risus risus, dignissim quis porttitor a, suscipit in nulla. Vestibulum tempor auctor elit eu sollicitudin. Donec aliquam a magna in gravida. Sed luctus mattis tellus, id rutrum magna ornare eu. Vestibulum imperdiet velit non ante feugiat tempus. Sed rutrum augue at sem tempus sodales. Vestibulum dapibus mauris quam, sed dictum quam volutpat eget. Donec congue convallis lacus, et sagittis sem consequat non. In tortor leo, viverra id turpis at, fermentum ultricies tortor. Donec egestas, magna a dictum commodo, risus ligula pulvinar odio, sed sagittis arcu odio id massa.
-        </p>	
+<div class="container">
+    <div class="col-md-6 col-xs-12">
+
+        <h3>Geef hier je aanvraag in</h3><br>
+
+        <form class="centerBericht" method="POST" action="">
+            <div>
+            {{csrf_field()}}
+                <label id="type" for="typeSelect">Type: </label>
+                <select id="typeSelect">
+                    <option value="0">Selecteer een optie:</option>
+                    <option value="1">Plugin</option>
+                    <option value="2">Mod</option>
+                    <option value="3">Programeren</option>
+                    <option value="4">Andere...</option>
+                </select>
+            </div>
+            <br/>
+            <!-- <button type="submit" value="Submit" style="margin-bottom: 20px;" class="submitButton">Voeg aankondiging toe</button> -->
+        </form>
+
     </div>
 
-    <div class="row col-sm-6">
+    <div class=" col-md-6 col-xs-12">
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis imperdiet sapien, viverra suscipit ex. Quisque semper nunc bibendum purus suscipit tincidunt. Cras auctor nisl eu dolor imperdiet semper. In pretium malesuada dignissim. Nulla facilisi. Mauris a ligula rutrum, maximus dolor in, tincidunt turpis. Vestibulum risus risus, dignissim quis porttitor a, suscipit in nulla. Vestibulum tempor auctor elit eu sollicitudin. Donec aliquam a magna in gravida. Sed luctus mattis tellus, id rutrum magna ornare eu. Vestibulum imperdiet velit non ante feugiat tempus. Sed rutrum augue at sem tempus sodales. Vestibulum dapibus mauris quam, sed dictum quam volutpat eget. Donec congue convallis lacus, et sagittis sem consequat non. In tortor leo, viverra id turpis at, fermentum ultricies tortor. Donec egestas, magna a dictum commodo, risus ligula pulvinar odio, sed sagittis arcu odio id massa.
         </p>	
